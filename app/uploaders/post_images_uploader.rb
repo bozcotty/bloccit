32099d77acd95de6a7ee516bd9cb37811b207d3e
Scott Hale <shalemont@gmail.com>
2013-11-05 10:52:43 -0700

    Created user profile

--- app/uploaders/post_images_uploader.rb
+++ app/uploaders/post_images_uploader.rb
@@ -35,6 +35,10 @@ class PostImagesUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  version :small do
+  process :resize_to_fill => [130, 130]
+  end
+
   # Create different versions of your uploaded files:
   version :thumb do
   process :resize_to_fill => [50, 50]
