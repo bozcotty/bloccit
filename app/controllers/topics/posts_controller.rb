dcff0c1e7522708a48be3fe67c466153a70e309b
Scott Hale <shalemont@gmail.com>
2013-11-06 13:30:51 -0700

    Created Popular Posts and Users, and optimized topics#show

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -1,8 +1,5 @@
-class PostsController < ApplicationController
+class Topics::PostsController < ApplicationController
   
-  def index
-    @posts = Post.visible_to(current_user).where("posts.created_at > ?", 7.days.ago).paginate(page: params[:page], per_page: 10)
-  end
 
   def show
     @topic = Topic.find(params[:topic_id])
